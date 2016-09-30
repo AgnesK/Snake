d72f85efa772c70cd1bbac7c1a829b18a11fa417
Agnes Köhler <nessi.is.da@gmail.com>
2016-09-30 18:36:14 +0100

    initial commit with basic snake implementation

---
+++ Code/Assets/Scripts/Snake.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+
+	// Current Movement Direction
+	// (by default it moves to the right)
+	public Vector2 direction = Vector2.right;
+	public float movementSpeed;
+
+	// Keep Track of Tail
+	List<Transform> tail = new List<Transform>();
+
+	// Did the snake eat something?
+	bool ate = false;
+
+	// Tail Prefab
+	public GameObject tailPrefab;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Move", 0.05f, movementSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Move in a new Direction?
+		if (Input.GetKey (KeyCode.RightArrow))
+			direction = Vector2.right;
+		else if (Input.GetKey (KeyCode.DownArrow))
+			direction = Vector2.down;
+		else if (Input.GetKey (KeyCode.LeftArrow))
+			direction = Vector2.left;
+		else if (Input.GetKey (KeyCode.UpArrow))
+			direction = Vector2.up;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		// Food?
+		if (collider.name.StartsWith ("Food")) {
+			// Get longer in next Move call
+			ate = true;
+
+			// Remove the Food
+			Destroy (collider.gameObject);
+		} 
+		// Collided with tail or Border
+		else {
+			// ToDo 'You lose' screen
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+
+	void Move() {
+		// Save current position of head
+		Vector2 lastHeadPosition = transform.position;
+
+		// Move head into new direction
+		transform.Translate(direction);
+
+		// Ate something? Then insert new Element into gap
+		if (ate) {
+			// Load Prefab into the world
+			GameObject snakeAppendix = (GameObject)Instantiate (tailPrefab, lastHeadPosition, Quaternion.identity);
+
+			// Keep track of it in our tail list
+			tail.Insert (0, snakeAppendix.transform);
+
+			// Reset the flag
+			ate = false;
+		}
+		// Do we have a Tail?
+		else if (tail.Count > 0) {
+			// Move last Tail Element to where the Head was
+			tail.Last ().position = lastHeadPosition;
+
+			// Add to front of list, remove from the back
+			tail.Insert (0, tail.Last ());
+			tail.RemoveAt (tail.Count - 1);
+		}
+	}
+}
---
+++ Code/Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour {
+
+	// food prefab
+	public GameObject food;
+
+	// borders
+	public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+	// snake
+	public GameObject snake;
+
+	// Use this for initialization
+	void Start () {
+		// Spawn food every 4 seconds, starting in 3
+		InvokeRepeating("Spawn", 3, 4);
+	}
+
+	void Update() {
+
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log ("I clicked somewhere");
+			// get position of click
+			Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log (clickPosition);
+
+			// Check where the snake is standing and going
+			Vector2 snakePosition = snake.transform.position;
+			Vector2 snakeDirection = snake.GetComponent<Snake>().direction;
+
+			// Check if you are going vertically or horizontally
+			// snake is going left or right
+			if (snakeDirection == Vector2.right || snakeDirection == Vector2.left) {
+				// click above the snake to go up
+				if (snakePosition.y < clickPosition.y) {
+					snake.GetComponent<Snake> ().direction = Vector2.up;
+				} // click below the snake to go down
+				else {
+					snake.GetComponent<Snake> ().direction = Vector2.down;
+				}
+			} // snake is going up or down
+			else {
+				// click right the snake to go right
+				if (snakePosition.x < clickPosition.x) {
+					snake.GetComponent<Snake> ().direction = Vector2.right;
+				} // click left of the snake to go left
+				else {
+					snake.GetComponent<Snake> ().direction = Vector2.left;
+				}
+			}
+		}
+	}
+
+	void Spawn() {
+		// x position between left & right border
+		int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+
+		// y position between top & bottom border
+		int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
+
+		// Instantiate the food at (x, y)
+		Instantiate(food, new Vector2(x, y), Quaternion.identity);
+	}
+}
